f40f048adcf8d822c7a16d1af22b5d2cb35284de
Joseph Frazier <joseph@onsip.com>
2015-01-12 02:51:26 -0500

    add initial implementation

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var denodeify = require('denodeify');
+var xhr = require('xhr');
+var xhrPromise = denodeify(xhr);
+
+module.exports = jsonproxy;
+
+function jsonproxy (host, url) {
+  if (!url) {
+    url = host;
+    host = null;
+  }
+  host = host || 'https://jsonp.nodejitsu.com/';
+
+  var xhrOptions = {
+    uri: host + '?url=' + encodeURIComponent(url),
+    json: true
+  };
+
+  return xhrPromise(xhrOptions)
+    .then(function (response) {
+      var body = response.body;
+      if (!/^2/.test(response.statusCode)) {
+        throw body.error;
+      }
+      else {
+        return body;
+      }
+    })
+  ;
+}
